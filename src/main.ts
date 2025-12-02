33693207abe4381741eedeb48e72e0dc61e1713e
Steve Jones <jonesst2608@gmail.com>
2025-12-02 07:02:03 +0000

    VSCODE debugger launch & breakpoints now working

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,8 @@ const createWindow = () => {
   // Load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    // Open DevTools in development mode
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(
       path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
